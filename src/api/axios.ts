e3c3481d4409018e4eb41a39eb9fb95705819ba9
pinchas lifshitz <pinchas.yesodot@gmail.com>
2026-05-10 14:08:15 +0300

    const cartItemsCount = cartItems.reduce((acc, item) => acc + item.quantity, 0);

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://server-side-assignment-c9gx.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
